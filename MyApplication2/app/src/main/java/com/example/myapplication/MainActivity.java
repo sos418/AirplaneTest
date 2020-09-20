281c0f11c48667780c1bcf4a7a808afa02d0fab1
sos418 <sos84418@gmail.com>
2020-09-20 23:51:10 +0800

    first commit

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "AirplaneModeActivity";
+    ToggleButton toggleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleBtn = (ToggleButton) findViewById(R.id.toggle_btn);
+        toggleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFlightMode(MainActivity.this);
+            }
+
+            public void setFlightMode(Context context) {
+                    // API 16 and earlier.
+                    boolean enabled = isFlightModeEnabled(context);
+                    Log.i(TAG, "API 16 : " + enabled);
+                    Settings.System.putInt(context.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, enabled ? 0 : 1);
+                    Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+                    intent.putExtra("state", !enabled);
+                    sendBroadcast(intent);
+            }
+
+            @SuppressLint("NewApi")
+            @SuppressWarnings("deprecation")
+            private boolean isFlightModeEnabled(Context context) {
+                boolean mode = false;
+                // API 16 and earlier.
+                mode = Settings.System.getInt(context.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1;
+                return mode;
+            }
+
+        });
+    }
+}
